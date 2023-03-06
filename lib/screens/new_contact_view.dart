9af706b9863bcb47352425be8f3b6c688d5ae8b7
Awisa Destiny <destinyawisa@gmail.com>
2023-03-07 00:12:31 +0100

    added a border to textfield and more decoration

--- lib/screens/new_contact_view.dart
+++ lib/screens/new_contact_view.dart
@@ -31,29 +31,39 @@ class _NewContactViewState extends State<NewContactView> {
       appBar: AppBar(
         title: const Text('Add New Contact'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-            controller: nameController,
-            decoration: const InputDecoration(label: Text('Enter New Contact')),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          MaterialButton(
-            color: Colors.deepPurple,
-            onPressed: () {
-              final Contact newContact = Contact(name: nameController.text);
-              ContactBook.singleton().addToContactList(newContact: newContact);
-              Navigator.of(context).pop();
-            },
-            child: const Text(
-              'Add contact',
-              style: TextStyle(color: Colors.white),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(
+                label: const Text('Enter Contact Name'),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                  borderSide: const BorderSide(width: 2),
+                ),
+              ),
             ),
-          )
-        ],
+            const SizedBox(
+              height: 20,
+            ),
+            MaterialButton(
+              color: Colors.deepPurple,
+              onPressed: () {
+                final Contact newContact = Contact(name: nameController.text);
+                ContactBook.singleton()
+                    .addToContactList(newContact: newContact);
+                Navigator.of(context).pop();
+              },
+              child: const Text(
+                'Add contact',
+                style: TextStyle(color: Colors.white),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
